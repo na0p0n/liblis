3575e87a4a2e6eb2ed3e03f5d59a9135cfd2960b
na0p0n <00naopon00@gmail.com>
2026-02-17 01:58:07 +0900

    ログ出力を追加

--- src/main/kotlin/net/naoponju/liblis/config/LoggingAspect.kt
+++ src/main/kotlin/net/naoponju/liblis/config/LoggingAspect.kt
@@ -1,25 +1,36 @@
 package net.naoponju.liblis.config
 
-import org.aspectj.lang.JoinPoint
-import org.aspectj.lang.annotation.AfterReturning
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
-import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-
 
 @Aspect
 @Component
 class LoggingAspect {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    @Before("execution(* net.naoponju.liblis.controller..*.*(..))")
-    fun logBefore(joinPoint: JoinPoint) {
-        logger.info("実行開始: ${joinPoint.signature.toShortString()}")
-    }
+    @Pointcut("execution(* net.naoponju.liblis.controller..*.*(..))")
+    fun controllerMethodExecution() {}
 
-    @AfterReturning("execution(* net.naoponju.liblis.controller..*.*(..))")
-    fun logAfter(joinPoint: JoinPoint) {
-        logger.info("実行完了: ${joinPoint.signature.toShortString()}")
+    @Around("controllerMethodExecution()")
+    fun logAround(joinPoint: ProceedingJoinPoint): Any? {
+        val signature = joinPoint.signature.toShortString()
+        logger.info("実行開始: $signature")
+
+        val start = System.currentTimeMillis()
+        try {
+            val result = joinPoint.proceed()
+            val executionTime = System.currentTimeMillis() - start
+            logger.info("実行完了: $signature (実行時間 : ${executionTime}ms)")
+
+            return result
+        } catch (e: Throwable) {
+            val executionTime = System.currentTimeMillis() - start
+            logger.error("実行時例外が発生: $signature (実行時間: ${executionTime}ms)", e)
+            throw e
+        }
     }
 }
