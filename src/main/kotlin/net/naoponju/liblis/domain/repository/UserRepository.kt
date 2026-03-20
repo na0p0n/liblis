31a5d8e450f95c9f3687e0fd76ad2e4c6bd09630
na0p0n <00naopon00@gmail.com>
2026-03-21 03:32:56 +0900

    コメント対応

--- src/main/kotlin/net/naoponju/liblis/domain/repository/UserRepository.kt
+++ src/main/kotlin/net/naoponju/liblis/domain/repository/UserRepository.kt
@@ -9,8 +9,6 @@ interface UserRepository {
 
     fun findByGoogleCredential(googleId: String): UserEntity?
 
-    fun findByAppleCredential(appleId: String): UserEntity?
-
     fun findByGitHubCredential(githubId: String): UserEntity?
 
     fun save(user: UserEntity)
@@ -25,16 +23,9 @@ interface UserRepository {
         githubId: String,
     )
 
-    fun addAppleCredentialById(
-        id: UUID,
-        appleId: String,
-    )
-
     fun clearGoogleCredentialByMailAddress(email: String)
 
     fun clearGithubCredentialByMailAddress(email: String)
-
-    fun clearAppleCredentialByMailAddress(email: String)
 
     fun deleteUser(userId: UUID)
 }
