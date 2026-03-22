bc57bf71373a9517bc930dd6609cb74df4447986
na0p0n <00naopon00@gmail.com>
2026-03-22 14:05:18 +0900

    楽天ブックスAPI対応

---
+++ src/main/kotlin/net/naoponju/liblis/infra/mapper/RakutenBooksGenreMapper.kt
@@ -0,0 +1,31 @@
+package net.naoponju.liblis.infra.mapper
+
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Param
+import org.apache.ibatis.annotations.Select
+
+@Mapper
+interface RakutenBooksGenreMapper {
+
+    @Select("""
+        SELECT books_genre_path
+        FROM v_rakuten_books_genre
+        WHERE books_genre_id = #{booksGenreId}
+        LIMIT 1
+    """)
+    fun findGenrePathById(booksGenreId: String): String?
+
+    @Select(
+        """
+            <script>
+                SELECT books_genre_id,
+                       COALESCE(books_genre_name_3, books_genre_name_2) AS display_genre_name
+                FROM v_rakuten_books_genre
+                WHERE books_genre_id IN
+                <foreach item="id" collection="ids" open="(" separator="," close=")">
+                    #{id}
+                </foreach>
+            </script>
+        """)
+    fun findGenreNamesByIds(@Param("ids") ids: List<String>): List<Map<String, String>>
+}
